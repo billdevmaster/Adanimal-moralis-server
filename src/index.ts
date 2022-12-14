08179929e355eadeced277ae7e33337c52d253cf
billdevmaster71 <billdevmaster@gmail.com>
2022-12-15 00:40:24 +0900

    fix: set correct abi

--- src/index.ts
+++ src/index.ts
@@ -43,7 +43,7 @@ app.post("/webhook", async (req, res) => {
     console.log(req.body.logs.length)
     for (let i = 0; i < req.body.logs.length; i++) {
       console.log(i)
-      const abi = req.body.abi;
+      const abi = req.body.abi[0];
       console.log(abi)
       if (abi) {
         const { filter, update } = realtimeUpsertParams(abi, req.body.logs[i], req.body.confirmed, req.body.block);
